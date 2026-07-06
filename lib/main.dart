c3dc0bcc8b1db8a988a38cb92960ee9423a9bad1
Rathish <rathishk24@gmail.com>
2026-07-06 17:01:26 +0530

    Added firebase core

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widget_previews.dart';
 import 'package:tutorial_management/ui/home_screen.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
